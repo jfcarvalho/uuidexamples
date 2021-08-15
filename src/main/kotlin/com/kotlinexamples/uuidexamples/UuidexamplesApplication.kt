4d1a61e901a17412452a7aa180f60f181410a269
jfcarvalho <jfcarvalho.profissional@gmail.com>
2021-08-15 17:07:49 -0300

    Subindo arquivos iniciais

---
+++ src/main/kotlin/com/kotlinexamples/uuidexamples/UuidexamplesApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlinexamples.uuidexamples
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UuidexamplesApplication
+
+fun main(args: Array<String>) {
+	runApplication<UuidexamplesApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlinexamples/uuidexamples/UuidexamplesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlinexamples.uuidexamples
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UuidexamplesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
